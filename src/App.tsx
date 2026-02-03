023b71d283fc0f9138a9cf11564fe5b68af12cb5
Thiago <thiagoports@hotmail.com>
2026-02-03 19:09:58 -0300

    linktree-intech

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { ProductCards } from './components/ProductCards';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-300 via-slate-200 to-cyan-200 p-8">
+      <ProductCards />
+    </div>
+  );
+}
